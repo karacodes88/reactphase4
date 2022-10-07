47184e03941b437e8cb04b3a0e0652bb0755f817
kara <karawhite59@gmail.com>
2022-10-07 11:20:00 +0100

    push

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div className="header">
+      <h1>Spice up your life!!!!</h1>
+      <img src="https://www.channel4fm.com/wp-content/uploads/2019/07/spice-girl.jpg"alt="spice girls" width={300}></img>
+    </div>
+  );
+};
+
+export default Header;
